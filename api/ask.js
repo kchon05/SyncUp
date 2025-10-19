c3843f90e6c41749de08dd6f5e14aa75f9784eae
kchon05 <kayliechon@gmail.com>
2025-10-19 06:51:15 -0700

    helpe[lwefwef

--- api/ask.js
+++ api/ask.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.AIzaSyAYmL9VLOt4NxSDkhTq4y38NI_Eu2LPVFo}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
